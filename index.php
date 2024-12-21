a306d65cd265a58df6c7a173413c120e059af071
FranPomba <francislevisspomba@gmail.com>
2024-12-21 16:55:53 +0100

    inicializando o repositorio

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/styles.css">
+    <title>Exemplo com Tailwind</title>
+</head>
+
+<body class="bg-slate-600 text-gray-800">
+    <div class="container mx-auto p-4">
+        
+    </div>
+</body>
+
+</html>
