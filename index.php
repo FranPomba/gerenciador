cb259c293a17b0a93a0ac73b302b8a58996ac701
FranPomba <francislevisspomba@gmail.com>
2024-12-21 21:40:02 +0100

    implementando o layout do site

--- index.php
+++ index.php
@@ -1,17 +1,82 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-Br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/styles.css">
-    <title>Exemplo com Tailwind</title>
+    <title>Meu Portifolio</title>
 </head>
 
-<body class="bg-slate-600 text-gray-800">
-    <div class="container mx-auto p-4">
-        
-    </div>
+<body class="bg-slate-900 text-gray-200">
+    <header class="mx-auto max-w-screen-lg items-center justify-between px-3 py-6 flex">
+        <div class="font-bold text-xl text-cyan-600">Meu Portifolio</div>
+        <div class="">
+            <ul class="flex gap-x-3 font-medium text-gray-200">
+                <li><a href="#" class="hover:underline">Projetos</a></li>
+                <li><a href="#" class="hover:underline">GitHub</a></li>
+                <li><a href="#" class="hover:underline">LinkedIn</a></li>
+                <li><a href="#" class="hover:underline">Twitter</a></li>
+            </ul>
+        </div>
+    </header>
+    <main class="mx-auto max-w-screen-lg min-h-20 px-3 py-6 gap-y-6">
+        <section class="flex gap-x-3">
+            <div class="w-2/3">
+                <h1 class="text-3xl font-bold">Olá, eu sou o Francisco!</h1>
+                <p class="text-xl leading-6 mt-6">Sou desenvolvedor Backend, adoro criar softwares e aprender novas tecnologias.
+                    Programo em PHP e HTML bem como possuo conhecimento de outras linguagens como Python e JavaScript.
+                </p>
+                <ul class="flex gap-x-3 mt-3">
+                    <li><a href="#">GitHub</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">LinkedIn</a></li>
+                    <li><a href="#">Facebook</a></li>
+                </ul>
+            </div>
+            <div class="w-1/3 ">
+                <div class="flex items-center justify-center">foto</div>
+            </div>
+        </section>
+
+        <section class=" space-y-3 py-6">
+            <h2 class="font-bold text-2xl">Projetos Recentes</h2>
+            <div class="bg-slate-800 rounded-lg p-3 flex items-center">
+                <div class="w-1/5 ">
+                    Projeto Web Design
+                </div>
+                <div class="w-4/5 space-y-3">
+
+                    <div class="flex gap-3 justify-between">
+                        <h3 class="font-semibold">Projeto 1
+                            <span class="text-xs text-gray-400 opacity-50 italic">(finalizado em 2024)
+                            </span>
+                        </h3>
+                        <div>
+                            <span class="bg-yellow-500 rounded-md px-2 py-1 font-semibold text-xs">PHP</span>
+                            <span class="bg-gray-700 rounded-md px-2 py-1 font-semibold text-xs">Tailwind</span>
+                            <span class="bg-red-500 rounded-md px-2 py-1 font-semibold text-xs">HTML</span>
+
+                        </div>
+                    </div>
+                    <p class="text-gray-500 text-sm leading-6">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Incidunt, quos sapiente, alias voluptatum quis repudiandae ad impedit molestias
+                        est id culpa rerum laborum officia voluptate eius repellat quod, iure perspiciatis?
+                    </p>
+
+
+                </div>
+
+            </div>
+        </section>
+    </main>
+    <footer class="mx-auto max-w-screen-lg min-h-20 ">
+
+        <div class="border-t border-gray-600 pt-6 px-3 text-gray-400">
+            <p>@ Copyright 2024. Feito pelo Francisco Pomba</p>
+        </div>
+    </footer>
 </body>
 
 </html>
